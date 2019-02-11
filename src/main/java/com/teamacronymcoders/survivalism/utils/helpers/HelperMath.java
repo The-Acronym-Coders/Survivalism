2274274eaa37a6963f275f9d7d1b7837b45b8274
Lanse <simon_stalnabb@hotmail.com>
2019-02-11 20:38:38 +0100

    Added a math method for clamping a float to the set decimals

--- src/main/java/com/teamacronymcoders/survivalism/utils/helpers/HelperMath.java
+++ src/main/java/com/teamacronymcoders/survivalism/utils/helpers/HelperMath.java
@@ -1,7 +1,15 @@
 package com.teamacronymcoders.survivalism.utils.helpers;
 
+import java.math.BigDecimal;
+
 public class HelperMath {
     public static boolean tryPercentage(double percent) {
         return Math.random() < percent;
+    }
+
+    public static BigDecimal round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd;
     }
 }
