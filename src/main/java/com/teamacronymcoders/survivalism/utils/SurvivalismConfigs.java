35cd47b6efc3a7068b2f866a5ef52c8d25192bb3
Lanse <simon_stalnabb@hotmail.com>
2019-02-12 21:50:43 +0100

    Added Config Option to show either Time or Ticks in JEI

--- src/main/java/com/teamacronymcoders/survivalism/utils/SurvivalismConfigs.java
+++ src/main/java/com/teamacronymcoders/survivalism/utils/SurvivalismConfigs.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class SurvivalismConfigs {
     public static boolean crtVerboseLogging;
     public static double baseJumpValue;
+    public static boolean timeOrTicks;
 
     private static ConfigRegistry configRegistry = Survivalism.INSTANCE.getRegistry(ConfigRegistry.class, "CONFIG");
 
@@ -17,15 +18,19 @@ public class SurvivalismConfigs {
         getValues();
     }
 
+
     private static void loadConfigs() {
         ConfigEntry crtVerboseLogging = new ConfigEntry("crafttweaker", "crtVerboseLogging", Property.Type.BOOLEAN, "false", "Enables Cleaner Verbose Logging for Crafttweaker Support", false);
         ConfigEntry baseJumpValue = new ConfigEntry("crushing_vat", "baseJumpValue", Property.Type.DOUBLE, "1.0", "Base Value per Jump for the Crushing Vat", false);
+        ConfigEntry TimeOrTicks = new ConfigEntry("JEI", "TimeOrTicks", Property.Type.BOOLEAN, "false", "Set to true if it should display time in HH:MM:SS instead of Ticks?");
         configRegistry.addEntry(crtVerboseLogging);
         configRegistry.addEntry(baseJumpValue);
+        configRegistry.addEntry(TimeOrTicks);
     }
 
     private static void getValues() {
         baseJumpValue = configRegistry.getDouble("baseJumpValue", 1.0);
         crtVerboseLogging = configRegistry.getBoolean("crtVerboseLogging", false);
+        timeOrTicks = configRegistry.getBoolean("TimeOrTicks", false);
     }
 }
