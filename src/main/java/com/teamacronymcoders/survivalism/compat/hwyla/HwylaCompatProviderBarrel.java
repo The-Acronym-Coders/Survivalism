4aacc422e969f800fc8cb2f58abd627e96d9ee7a
Lanse <simon_stalnabb@hotmail.com>
2019-02-14 00:30:24 +0100

    Started work on Hwyla Compat

---
+++ src/main/java/com/teamacronymcoders/survivalism/compat/hwyla/HwylaCompatProviderBarrel.java
@@ -0,0 +1,50 @@
+package com.teamacronymcoders.survivalism.compat.hwyla;
+
+import com.teamacronymcoders.survivalism.common.blocks.BlockBarrel;
+import com.teamacronymcoders.survivalism.common.tiles.TileBarrel;
+import com.teamacronymcoders.survivalism.utils.helpers.HelperString;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fluids.FluidTank;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class HwylaCompatProviderBarrel implements IWailaDataProvider {
+    @Nonnull
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        IBlockState state = accessor.getBlockState();
+        TileEntity te = accessor.getTileEntity();
+
+        if (config.getConfig("survivalism.barrel")) {
+            if (accessor.getBlock() instanceof BlockBarrel && te instanceof TileBarrel) {
+                FluidTank input = ((TileBarrel) te).getInput();
+                FluidTank output = ((TileBarrel) te).getOutput();
+                if (input != null) {
+                    if (input.getFluid() == null ) {
+                         currenttip.add(I18n.format("survivalism.hwyla.tank.input.empty"));
+                    } else {
+                        currenttip.add(I18n.format("survivalism.hwyla.tank.input") + " " + input.getFluid().getLocalizedName() + " : " + input.getFluid().amount + "/" + input.getCapacity());
+                    }
+                }
+                if (output != null) {
+                    if (output.getFluid() == null) {
+                        currenttip.add(I18n.format("survivalism.hwyla.tank.output.empty"));
+                    } else {
+                        currenttip.add(I18n.format("survivalism.hwyla.tank.output") + " " + output.getFluid().getLocalizedName() + " : " + output.getFluid().amount + "/" + output.getCapacity());
+                    }
+                }
+                currenttip.add(I18n.format("survivalism.hwyla.barrel.state") + " " + HelperString.cleanBarrelStateString(String.valueOf(state.getValue(BlockBarrel.BARREL_STATE))));
+                currenttip.add(I18n.format("survivalism.hwyla.barrel.sealed") + " " + state.getValue(BlockBarrel.SEALED));
+            }
+        }
+
+        return currenttip;
+    }
+}
---
+++ src/main/java/com/teamacronymcoders/survivalism/compat/hwyla/HwylaCompatProviderCrushingVat.java
@@ -0,0 +1,48 @@
+package com.teamacronymcoders.survivalism.compat.hwyla;
+
+import com.teamacronymcoders.survivalism.common.blocks.BlockBarrel;
+import com.teamacronymcoders.survivalism.common.blocks.BlockCrushingVat;
+import com.teamacronymcoders.survivalism.common.tiles.TileBarrel;
+import com.teamacronymcoders.survivalism.common.tiles.TileCrushingVat;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fluids.FluidTank;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+public class HwylaCompatProviderCrushingVat implements IWailaDataProvider {
+
+    @Nonnull
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        TileEntity te = accessor.getTileEntity();
+
+        if (config.getConfig("survivalism.crushing_vat")) {
+            if (accessor.getBlock() instanceof BlockCrushingVat && te instanceof TileCrushingVat) {
+                ItemStack input = ((TileCrushingVat) te).getInputInv().getStackInSlot(0);
+                FluidTank tank = ((TileCrushingVat) te).getTank();
+                ItemStack output = ((TileCrushingVat) te).getOutputInv().getStackInSlot(0);
+                if (!input.isEmpty()) {
+                    currenttip.add(I18n.format("survivalism.hwyla.items.input") + " " + input.getDisplayName());
+                }
+
+                if (!output.isEmpty()) {
+                    currenttip.add(I18n.format("survivalism.hwyla.items.output") + " " + output.getDisplayName());
+                }
+
+                if (tank != null) {
+                    currenttip.add(I18n.format("survivalism.hwyla.tank.output") + " " + tank.getFluid());
+                }
+            }
+        }
+
+        return currenttip;
+    }
+}
