63a84d7777f2d63f0aac5b9ac6f992476f0836e9
Lanse <simon_stalnabb@hotmail.com>
2019-02-14 14:31:14 +0100

    Testing Update Method

--- src/main/java/com/teamacronymcoders/survivalism/common/tiles/TileBarrel.java
+++ src/main/java/com/teamacronymcoders/survivalism/common/tiles/TileBarrel.java
@@ -21,8 +21,12 @@ import net.minecraft.inventory.Container;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
@@ -40,6 +44,7 @@ public class TileBarrel extends TileBase implements ITickable, IUpdatingInventor
     public static final int TANK_CAPACITY = 16000;
     public static final int STORAGE_SIZE = 9;
 
+    protected int prevInputAmount = 0;
     protected FluidTank input = new FluidTank(TANK_CAPACITY);
     protected FluidTank output = new FluidTank(TANK_CAPACITY);
     protected ItemStackHandler inv = new BarrelHandler(STORAGE_SIZE, this);
@@ -70,6 +75,20 @@ public class TileBarrel extends TileBase implements ITickable, IUpdatingInventor
         if (poweredLastTick != powered) {
             updateSeal(powered);
         }
+        if (getInput().getFluidAmount() > 0) {
+            if (getInput().getFluidAmount() != prevInputAmount) {
+                prevInputAmount = getInput().getFluidAmount();
+                world.notifyBlockUpdate(getPos(), world.getBlockState(getPos()), world.getBlockState(getPos()), 8);
+            }
+        }
+
+        if (getOutput().getFluidAmount() > 0) {
+            if (getOutput().getFluidAmount() != prevInputAmount) {
+                prevInputAmount = getOutput().getFluidAmount();
+                world.notifyBlockUpdate(getPos(), world.getBlockState(getPos()), world.getBlockState(getPos()), 8);
+            }
+        }
+            
         poweredLastTick = powered;
         BarrelState state = getState();
         if (state == BarrelState.STORAGE || !isSealed()) {
@@ -181,6 +200,28 @@ public class TileBarrel extends TileBase implements ITickable, IUpdatingInventor
             return state.getValue(BlockBarrel.BARREL_STATE);
         }
         return BarrelState.STORAGE;
+    }
+
+    @Nullable
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(this.pos, 0, this.getUpdateTag());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        super.onDataPacket(net, pkt);
+        this.readFromNBT(pkt.getNbtCompound());
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return this.writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+        return oldState.getBlock() != newState.getBlock();
     }
 
     @Nullable
