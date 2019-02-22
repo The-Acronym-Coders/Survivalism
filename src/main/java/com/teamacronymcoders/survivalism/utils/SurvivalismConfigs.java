49c9ab6d954cc167737f50dad6149e6410d56ccd
Lanse <simon_stalnabb@hotmail.com>
2019-02-22 20:32:41 +0100

    Added config option to make it so the barrel can fill if its raining.

--- src/main/java/com/teamacronymcoders/survivalism/utils/SurvivalismConfigs.java
+++ src/main/java/com/teamacronymcoders/survivalism/utils/SurvivalismConfigs.java
@@ -11,6 +11,7 @@ public class SurvivalismConfigs {
     public static double baseJumpValue;
     public static boolean timeOrTicks;
     public static int rainFillRate;
+    public static boolean canBarrelsFillInRain;
 
     private static ConfigRegistry configRegistry = Survivalism.INSTANCE.getRegistry(ConfigRegistry.class, "CONFIG");
 
@@ -25,10 +26,12 @@ public class SurvivalismConfigs {
         ConfigEntry baseJumpValue = new ConfigEntry("crushing_vat", "baseJumpValue", Property.Type.DOUBLE, "1.0", "Base Value per Jump for the Crushing Vat", false);
         ConfigEntry timeOrTicks = new ConfigEntry("jei", "TimeOrTicks", Property.Type.BOOLEAN, "false", "Set to true if it should display time in HH:MM:SS instead of Ticks?");
         ConfigEntry rainFillRate = new ConfigEntry("barrel_general", "rainFillRate", Property.Type.INTEGER, "5", "The rate per tick which the barrel fills with water if it's raining and the barrel is left un-sealed to an open sky", false);
+        ConfigEntry canBarrelsFillInRain = new ConfigEntry("barrel_general", "canBarrelsFillInRain", Property.Type.BOOLEAN, "true", "Decides if the barrel should fill if it's raining and left open to the sky", false);
         configRegistry.addEntry(crtVerboseLogging);
         configRegistry.addEntry(baseJumpValue);
         configRegistry.addEntry(timeOrTicks);
         configRegistry.addEntry(rainFillRate);
+        configRegistry.addEntry(canBarrelsFillInRain);
     }
 
     private static void getValues() {
@@ -36,5 +39,6 @@ public class SurvivalismConfigs {
         crtVerboseLogging = configRegistry.getBoolean("crtVerboseLogging", false);
         timeOrTicks = configRegistry.getBoolean("timeOrTicks", false);
         rainFillRate = configRegistry.getInt("rainFillRate", 5);
+        canBarrelsFillInRain = configRegistry.getBoolean("canBarrelsFillInRain", true);
     }
 }
