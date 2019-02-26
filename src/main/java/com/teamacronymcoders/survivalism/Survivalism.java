13918463c4585a7f25048d0b597045daf4fe48b0
Lanse <simon_stalnabb@hotmail.com>
2019-02-26 21:17:17 +0100

    FML

--- src/main/java/com/teamacronymcoders/survivalism/Survivalism.java
+++ src/main/java/com/teamacronymcoders/survivalism/Survivalism.java
@@ -34,7 +34,7 @@ public class Survivalism extends BaseModFoundation<Survivalism> {
     public static final String NAME = "Survivalism";
     public static final String VERSION = "1.12.2-1.0.0";
     public static final String MCVERSION = "1.12,";
-    public static final String DEPS = "required-after:base;" + "before:patchouli;";
+    public static final String DEPS = "required-after:base;" + "after:patchouli;";
     private static final String COMMON = "com.teamacronymcoders.survivalism.common.CommonProxy";
     private static final String CLIENT = "com.teamacronymcoders.survivalism.client.ClientProxy";
     @Mod.Instance(Survivalism.MODID)
--- src/main/java/com/teamacronymcoders/survivalism/compat/patchouli/PatchouliModule.java
+++ src/main/java/com/teamacronymcoders/survivalism/compat/patchouli/PatchouliModule.java
@@ -2,19 +2,27 @@ package com.teamacronymcoders.survivalism.compat.patchouli;
 
 import com.teamacronymcoders.base.modulesystem.Module;
 import com.teamacronymcoders.base.modulesystem.ModuleBase;
+import com.teamacronymcoders.base.modulesystem.dependencies.IDependency;
+import com.teamacronymcoders.base.modulesystem.dependencies.ModDependency;
 import com.teamacronymcoders.survivalism.Survivalism;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import vazkii.patchouli.api.PatchouliAPI;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.function.Supplier;
 
 @Module(Survivalism.MODID)
 public class PatchouliModule extends ModuleBase {
+
+    @Override
+    public List<IDependency> getDependencies(List<IDependency> dependencies) {
+        dependencies.add(new ModDependency("patchouli"));
+        return super.getDependencies(dependencies);
+    }
 
     @Override
     public String getName() {
